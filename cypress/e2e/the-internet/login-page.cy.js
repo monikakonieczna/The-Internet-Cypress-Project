199cc10d3799e9e099a43edabb65c0f3d81f55d7
Monika Konieczna <Monika.Konieczna@itechart-group.com>
2022-11-17 16:50:24 +0100

    Fixture with incorrect credentials. Login page spec file.

--- cypress/e2e/the-internet/login-page.cy.js
+++ cypress/e2e/the-internet/login-page.cy.js
@@ -6,12 +6,25 @@ describe('Test the login page', () => {
     })
 
     it('Login with correct credentials.', () => {
-        cy.get('#username').type(JSON.stringify(Cypress.env('login'), {log: false}))
-        cy.get('#password').type(Cypress.env('password'), {log: false})
-        cy.get('[type="submit"]').click()
+        cy.fixture('cre').then((cre) => {
+            cy.get('#username').clear().type(cre.login);
+            cy.get('#password').clear().type(cre.password);
+        });
+        cy.get('[type="submit"]').click();
+        cy.url().should('include', 'secure');
+        cy.get('#flash').should('have.class', 'flash success');
+        cy.get('h2').should('include.text', ' Secure Area');
     })
 
     it('Login with incorrect credentials. Should show error message.', () => {
-        
+        cy.fixture('wrongcre').then((cre) => {
+            cy.get('#username').clear().type(cre.login, { log: false });
+            cy.get('#password').clear().type(cre.password, { log: false });
+        });
+
+        cy.get('[type="submit"]').click();
+        cy.url().should('not.include', 'secure');
+        cy.get('#flash').should('have.class', 'flash error');
+        cy.get('h2').should('include.text', 'Login Page');
     })
 })
